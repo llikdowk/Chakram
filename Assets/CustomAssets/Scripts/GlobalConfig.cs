007c234fad3af8157d24711d062bab5f1bd3941a
Llikdowk <guillermofm7@gmail.com>
2017-03-07 08:15:44 +0100

    add C5 & Promises libraries

---
+++ Assets/CustomAssets/Scripts/GlobalConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using RSG;
+using C5;
+
+public class GlobalConfig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
