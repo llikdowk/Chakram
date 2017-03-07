36b3efc77e04dde8880a70f88a82ff8f734c23b6
Llikdowk <guillermofm7@gmail.com>
2017-03-07 13:59:22 +0100

    add background

--- Assets/CustomAssets/Scripts/GlobalConfig.cs
+++ Assets/CustomAssets/Scripts/GlobalConfig.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
-using RSG;
-using C5;
 
 public class GlobalConfig : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public void Update() {
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public void OnGUI() {
 		
+		foreach (var touch in Input.touches) {
+			var message = "";
+			message += "ID: " + touch.fingerId + "\n";
+			message += "Phase: " + touch.phase + "\n";
+			message += "TapCount: " + touch.tapCount + "\n";
+			message += "Pos X: " + touch.position.x + "\n";
+			message += "Pos Y: " + touch.position.y + "\n";
+
+			int num = touch.fingerId;
+			GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
+		}
 	}
 }
