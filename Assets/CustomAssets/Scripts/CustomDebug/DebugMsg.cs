4f013f411eba3258738cddb2628687d30f973538
Llikdowk <guillermofm7@gmail.com>
2017-03-08 12:45:25 +0100

    add DebugMsg

---
+++ Assets/CustomAssets/Scripts/CustomDebug/DebugMsg.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CustomDebug {
+	public static class DebugMsg {
+		public delegate void DebugLog(string msg);
+
+		private const string sep = " <color=grey>##</color> ";
+
+		public static void ComponentNotFound(DebugLog log, System.Type component, string extraInfo = "") {
+			log("Component <b>" + component + "</b> not found" + sep + extraInfo);
+		}
+
+		public static void GameObjectNotFound(DebugLog log, string gameObjectName, string extraInfo = "") {
+			log("GameObject with name <b>" + gameObjectName + "</b> not found" + sep + extraInfo);
+		}
+
+		public static void NoExistantInteraction(DebugLog log) {
+			log("Trying to remove a non-existant interaction");
+		}
+
+		public static void SingletonNotCreated(DebugLog log) {
+			log("Singleton has not been created!");
+		}
+
+		public static void ChildObjectNotFound(DebugLog log, string childObjectName) {
+			log("Child object <b>" + childObjectName + "</b> not found in current gameObject");
+		}
+	}
+}
