880e82c10eb0d2e403d17a6cd3b7b687171881d4
Llikdowk <guillermofm7@gmail.com>
2017-03-13 12:02:20 +0100

    refine line rail

---
+++ Assets/CustomAssets/Scripts/Game/GameState.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+namespace Game {
+	public static class GameState {
+		public static void GameOver() {
+			Debug.Log("gameover stuff");
+		}
+	}
+}
--- Assets/CustomAssets/Scripts/Game/Player.cs
+++ Assets/CustomAssets/Scripts/Game/Player.cs
@@ -6,6 +6,7 @@ namespace Game {
 		private new CircleCollider2D collider;
 		private int layerMaskAllButPlayer;
 		private int lineLayer;
+		public float Speed = 4.5f;
 
 		public void Awake() {
 			collider = GetComponent<CircleCollider2D>();
@@ -14,40 +15,34 @@ namespace Game {
 		}
 
 		public void Update() {
-			CheckCollision(Physics2D.OverlapCircle(transform.position, collider.radius * transform.localScale.x, layerMaskAllButPlayer));
-			transform.position += Vector3.up * 3.0f * Time.deltaTime;
+			if (!CheckCollision(Physics2D.OverlapCircle(transform.position, collider.radius * transform.localScale.x,
+					layerMaskAllButPlayer))) 
+			{
+				transform.position += Vector3.up * Speed * Time.deltaTime;
+			}
 
 		}
 
-		private bool first = true;
-		private void CheckCollision(Collider2D hitCollider) {
-			if (!hitCollider) {
-				first = true;
-				return;
-			}
+		private bool CheckCollision(Collider2D hitCollider) {
+			if (!hitCollider) return false;
+
 			if (hitCollider.gameObject.layer == lineLayer) {
 				Line line = hitCollider.gameObject.GetComponent<Line>();
 				Vector3 p = line.Vertices[0];
 				Vector3 q = line.Vertices[1];
-				Vector3 v = (q - p).normalized;
-				/*
-				if (first) {
-					Vector3 c = gameObject.transform.position;
-					float lx = (c.x - p.x) / (v.x - 0.0f);
-					float ly = (c.y - p.y) / (v.y - 1.0f);
-					Vector2 magnetPoint = new Vector2(p.x + lx * v.x, p.y + ly * v.y);
-					gameObject.transform.position = magnetPoint;
-					first = false;
+				Vector3 d = q - transform.position;
+				Vector3 v = d.normalized;
+				Debug.Log(d.sqrMagnitude);
+				if (d.sqrMagnitude < 0.5f) {
+					return false;
 				}
-				*/
 				gameObject.transform.position += v * Time.deltaTime * 9.0f;
-
+				return true;
 			}
 			else {
-				first = true;
-				Debug.Log("game over");
+				GameState.GameOver();
+				return false;
 			}
-
 		}
 	}
 }
