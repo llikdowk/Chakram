fc4892fb9d7370e66e38c3e54395ea3406b33f6d
Llikdowk <guillermofm7@gmail.com>
2017-03-16 15:38:57 +0100

    refine movement

--- Assets/CustomAssets/Scripts/Game/Player.cs
+++ Assets/CustomAssets/Scripts/Game/Player.cs
@@ -6,9 +6,10 @@ namespace Game {
 		private new CircleCollider2D collider;
 		private int layerMaskAllButPlayer;
 		private int lineLayer;
-		public float Speed = 10.0f;
-		public float RailSpeed = 15.0f;
+		public float Speed = 12.5f;
+		public float RailSpeed = 17.5f;
 		public static Player Instance;
+		private Vector3 inheritedDirection = Vector3.up;
 
 		public void Awake() {
 			if (Instance == null) {
@@ -23,11 +24,13 @@ namespace Game {
 			}
 		}
 
+		private const float inheritedHorizontalMultiplier = 3.0f;
+		private const float inheritedLag = 8.5f;
 		public void Update() {
 			if (!CheckCollision(Physics2D.OverlapCircle(transform.position, collider.radius * transform.localScale.x,
-					layerMaskAllButPlayer))) 
-			{
-				transform.position += Vector3.up * Speed * Time.deltaTime;
+					layerMaskAllButPlayer))) {
+				transform.position += Vector3.up * Speed * Time.deltaTime + inheritedDirection * Time.deltaTime + Vector3.Project(inheritedDirection, Vector3.right) * inheritedHorizontalMultiplier * Time.deltaTime;
+				inheritedDirection = Vector3.Lerp(inheritedDirection, Vector3.zero, Time.deltaTime * inheritedLag);
 			}
 
 		}
@@ -40,12 +43,14 @@ namespace Game {
 				Vector3 p = line.Vertices[0];
 				Vector3 q = line.Vertices[1];
 				Vector3 d = q - p;//transform.position;
+				Vector2 dplayer = transform.position - p;
 				Vector3 v = d.normalized;
 				//Debug.Log(d.sqrMagnitude);
 				if (d.sqrMagnitude < 0.1f) {
 					return false;
 				}
-				gameObject.transform.position += v * Time.deltaTime * RailSpeed;
+				inheritedDirection = d;
+				gameObject.transform.position += v * Time.deltaTime * RailSpeed;// + (Vector3)(dplayer);
 				return true;
 			}
 			else {
