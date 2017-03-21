df344d485e7a0449312c8337fd1d9a8ca2f53e3c
Llikdowk <guillermofm7@gmail.com>
2017-03-21 21:47:01 +0100

    fix circle wave glitch

--- Assets/CustomAssets/Scripts/Visual/CircleWave.cs
+++ Assets/CustomAssets/Scripts/Visual/CircleWave.cs
@@ -5,7 +5,7 @@ namespace Visual {
 		public int NumCircles = 2;
 		public Color Color = Color.white;
 		private MeshRenderer renderer;
-		private float toffset = 0.0f;
+		private float elapsedTime = 0.0f;
 
 		public void Awake() {
 			var matCircle = new Material(Shader.Find("Custom/Circle"));
@@ -16,13 +16,12 @@ namespace Visual {
 				circles[i].color = Color;
 			}
 			renderer.materials = circles;
-			toffset = Time.time - (int) Time.time;
 		}
 
 		public void Update() {
-			float t = Time.time - toffset;
+			elapsedTime += Time.deltaTime;
 			for (int i = 0; i < renderer.materials.Length; ++i) {
-				float taux = t + i * 0.25f;
+				float taux = elapsedTime + i * 0.25f;
 				float tfraction = taux - (int) taux; 
 				renderer.materials[i].SetFloat("_Radius", 0.5f * tfraction);
 			}
@@ -30,7 +29,6 @@ namespace Visual {
 
 		public void OnEnable() {
 			renderer.enabled = true;
-			toffset = Time.time - (int) Time.time;
 		}
 
 		public void OnDisable() {
